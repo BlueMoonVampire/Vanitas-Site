42ff85d179eb12274824f04f71a175bcf8b514a8
Pranav Ajay <86167689+MoeZilla@users.noreply.github.com>
2022-02-04 18:14:20 +0530

    Create app.js

---
+++ app.js
@@ -0,0 +1,12 @@
+export default {
+  github: 'https://github.com/BlueMoonVampire',
+  docsRepositoryBase: 'https://github.com/BlueMoonVampire/Vanitas-Site',
+  head: (
+    <>
+      <script data-name="BMC-Widget" data-cfasync="false" src="https://cdnjs.buymeacoffee.com/1.0.0/widget.prod.min.js" data-id="vanitassystem" data-description="Support me on Buy me a coffee!" data-message="Vanitas Donate Please" data-color="#12fced" data-position="Left" data-x_margin="15" data-y_margin="15"></script> 
+   </>  
+  ),
+  footer: true,
+  footerEditLink: 'Github Page',
+  footerText: <>Copyright Vanitas</>
+}
